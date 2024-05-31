5fc0d6f6f7a4387178a2242691c49fc12a56d73a
Dominic Tigreros-Bosini <162226392+Dom-Tigre@users.noreply.github.com>
2024-05-31 22:29:37 +0100

    Update sorting.py
    Made bubble sort more efficient, prevents unnecesary passes

--- sorting.py
+++ sorting.py
@@ -14,9 +14,14 @@ class Sort:
 
         n = len(arr)
         for i in range(n):
+            noSwaps = True
             for j in range(n - i - 1):
                 if arr[j] < arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1], arr[j]
+                    noSwaps = False
+
+            if noSwaps:
+                break
 
         return arr
 
